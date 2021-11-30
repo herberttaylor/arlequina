5233197b88b0739ffbeefb46fcd6ec70adf3f749
herberttaylor <95135057+herberttaylor@users.noreply.github.com>
2021-11-30 19:14:16 -0300

    Initial commit

---
+++ instalação.js
@@ -0,0 +1,39 @@
+Instalar a arlequina bot.
+
+Primeiro extraia a pasta pro armazenamento interno.
+Em seguida, abra seu termux(ou outro terminal)
+
+Digite os seguintes comandos separadamente no termux:
+
+termux-setup-storage
+(no comando acima ele ira pedir acesso ao seus arquivos internos, aceite)
+
+pkg install
+pkg update
+pkg upgrade
+pkg install bash
+pkg install npm
+apt install
+apt update
+apt upgrade
+
+
+*Sua instalação inicial foi feita*
+
+Agora para instalar o bot, digite os comandos abaixo separadamente
+
+cd /sdcard
+ls 
+(no ls ele ira mostrar todos seus arquivos, procure o nome do arquivo alerquina)
+Caso esteja no jeito padrão, use o comando abaixo
+
+cd ryan
+(se aparecer no such file on directory, procure o nome da pasta arlequina)
+
+(caso de sucesso e encontre o diretorio, digite os comandos abaixo)
+
+bash install.sh
+
+npm start
+
+Depois so conectar whastapp web do seu bot
---
+++ src/help.js
@@ -0,0 +1,160 @@
+const help = (pushname,prefix) => {
+return`
+👋 Olá ${pushname}
+╭───────────⌁
+│   _*𝙈𝙀𝙉𝙐 𝘿𝘼 𝘼𝙇𝙀𝙍𝙌𝙐𝙄𝙉𝘼*_ 
+╘┬──────────⌁
+┌┤🌹 *_𝙁𝙄𝙂𝙐𝙍𝙄𝙉𝙃𝘼𝙎_* 🌹
+│╰───────────┈┈✨
+│ ❧ *?f*
+│ ❧ *?sticker*
+│ ❧ *?figu*
+│ ❧ *?attp*
+│ ❧ *?attp1 ao 6 (não funfa)*
+│ ❧ *?img (fazer a fig virar imagem*
+│ ❧ *?gif (fazer a fig virar gif)*     
+╰───────────┈┈ 
+┌───────────┈┈
+│ ⚒️ *_𝘼𝘿𝙈𝙎_* ⚒️
+╰───────────┈┈
+│ ❧ *?marcar (só sendo vip)*
+│ ❧ *?marcar2 (só dono usa)*
+│ ❧ *?add ex: +5531xxxxx*
+│ ❧ *?adc ex: marque a msg do membro   banido para adc ele dnv*
+│ ❧ *?ban marca a mensagem da pessoa   para ser banida*
+│ ❧ *?setname*
+│ ❧ *?setdesc*
+│ ❧ *?infogp (mostra as informações do grupo*
+│ ❧ *?linkgp (manda o link do grupo)*
+│ ❧ *?revoke (irá redefinir o link do grupo)*
+│ ❧ *?promover (irá da cargo de adm para um membro)*
+│ ❧ *?rebaixar (irá tirar o cargo de adm do membro)*
+│ ❧ *?abrirgp*
+│ ❧ *?fechargp*
+│ ❧ *?antifake [1/0] você vai ativar o antifake do grupo*
+│ ❧ *?antilink [1/0] você vai ativar o antilink do grupo*
+│ ❧ *se desativarem o antis esperem duas horas pra desativar completo*
+╰───────────┈┈
+┌───────────┈┈
+│ 🎵 *_𝙈𝙐𝙎𝙄𝘾𝘼𝙎_*🎵
+╰───────────┈┈
+│❧ *?play* *(Ex: ?play mtg aquelas coisas)* 
+│❧ *?virarmp3 (marque o vídeo para virar áudio)*
+╰───────────┈┈
+┌───────────┈┈
+│ 🔮 *_𝘾𝙊𝙉𝙎𝙐𝙇𝙏𝘼𝙎_*🔮
+╰───────────┈┈
+│❧ *?ddd*
+│❧ *?cep*
+│❧ *?ip*
+│❧ *?cnpj*
+│❧ *só funciona colocando números, não mencionando os membros do grupo*
+╰───────────┈┈
+┌───────────┈┈
+│ 🎯 _*𝘿𝙄𝙑𝙀𝙍𝙎𝘼̃𝙊*_🎯
+╰───────────┈┈
+│❧ *?rankgados*
+│❧ *?ranklindos*
+│❧ *?rankfeios*
+│❧ *?rankgays*
+│❧ *?ranknazistas*
+│❧ *?rankmacacos*
+│❧ *?rankgostosos*
+│❧ *?rankputa*
+│❧ *?ranklesbica*
+│❧ *?gostosas*
+│❧ *?gay*
+│❧ *?lesbica*
+│❧ *?lindo*
+│❧ *?gostoso*
+│❧ *?feio*
+│❧ *?casal*
+│❧ *?gado*
+│❧ *?cassino*
+│❧ *?meudinheiro*
+│❧ *?rankmaconha*
+│❧ *?ranktalaricos*
+│❧ *?rankrodados*
+│❧ *?ship*
+│❧ *?casar*
+│❧ *?talarico*
+╰───────────┈┈
+┌───────────┈┈
+│ 💸 _*𝙑𝙄𝙋𝙎*_💸
+╰───────────┈┈
+│❧ *a lista do free fire toda*
+│❧ *?rankmaconha*
+│❧ *?ranktalaricos*
+│❧ *?rankmacacos*
+│❧ *?marcar*
+│❧ *?cassino*
+│❧ *?talaricos*
+╰───────────┈┈
+┌───────────┈┈
+│ 🎤 _*𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙑𝙊𝙕*_🎤
+╰───────────┈┈
+│❧ *?oi*
+│❧ *?bomdia*
+│❧ *?boatarde*
+│❧ *?boanoite*
+│❧ *?gritar*
+│❧ *?vtmnc*
+│❧ *?meupau*
+╰───────────┈┈
+┌───────────┈┈
+│ 🔥 _*𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀*_🔥
+╰───────────┈┈
+│❧ *?addtime1* @
+│❧ *?addtime2* @
+│❧ *?addtime3* @
+│❧ *?addtime4* @
+│❧ *?cleartime1*
+│❧ *?cleartime2*
+│❧ *?cleartime3*
+│❧ *?cleartime4*
+│❧ *?time1mostrar*
+│❧ *?time2mostrar*
+│❧ *?time3mostrar*
+│❧ *?time4mostrar*
+│❧ *?addponts1*
+│❧ *?addponts2*
+│❧ *?addponts3*
+│❧ *?addponts4*
+│❧ *?timex4*
+│❧ *?criasala*
+╰───────────┈
+┌───────────┈┈
+│ 🌹 _*𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝙎𝙀𝙈 𝙋𝙍𝙀𝙁𝙄𝙓𝙊𝙎*_🌹
+│───────────┈┈
+│❧ *oi bot*
+│❧ *alerquina*
+│❧ *te amo alerquina*
+│❧ *bom dia*
+│❧ *boa tarde*
+│❧ *boa noite*
+│❧ *vtmnc bot*
+│❧ *vsfd bot*
+│❧ *bot fofa*
+│❧ *bot on?*
+│❧ *cade a bot*
+│❧ *obgd bot*
+╰───────────┈┈
+┌───────────┈┈
+│🪓 _*𝘿𝙊𝙉𝙊𝙎*_🪓
+╰───────────┈┈
+│❧ *?addvip*
+│❧ *?blocklist*
+│❧ *?setprefix*
+│❧ *?block*
+│❧ *?unblock*
+│❧ *?viplist*
+│❧ *?tm*
+│❧ *dono: ryan wa.me/5531971704128*
+│❧ *design e 2° dono: herbert wa.me/558194582007*
+╰───────────┈
+╰──────「 *ᴀʟᴇʀǫᴜɪɴᴀ* 」`
+}
+
+
+
+exports.help = help
